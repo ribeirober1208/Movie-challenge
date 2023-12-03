9209abd7899666be96efa129ded73ebeddc3b139
ribeirober1208 <ribeirober1208@gmail.com>
2023-12-03 14:21:16 -0300

    add_ENDPOINTS

--- src/services/tmdb.service.ts
+++ src/services/tmdb.service.ts
@@ -8,10 +8,18 @@ import { tap } from 'rxjs/operators';
 })
 export class TmdbService {
   private readonly _KEY = '5d900fd5a214dd86a0e9204cc6d49fa9';
-  private readonly _BASE_URL = 'https://api.themoviedb.org/3';
-  private readonly _ENDPOINT = '/movie/popular';
-  private readonly _DETAILS_ENDPOINT = '/movie';
-  private readonly _GENRES_ENDPOINT = '/genre/movie/list';
+  private readonly _BASE_URL = `https://api.themoviedb.org/3`;
+  private readonly _APPEND = `?include_adult=false&api_key=${this._KEY}&page=`
+  private readonly _ORDER = `/movie/popular`;
+  private readonly _DETAILS = `/movie`;
+  //alterar somente daqui para baixo, o trecho acima mantém a lista
+  private readonly _SORT = `&sort_by=`
+  private readonly _DISCOVER = `${this._BASE_URL}discover/movie${this._APPEND}` 
+  private readonly _ID = `${this._BASE_URL}movie`
+  private readonly _GENRES = `${this._BASE_URL}genre/movie/list`
+  private readonly _SELECTGENRE = `&with_genres=`
+  private readonly _SEARCH = `&with_keywords=`
+  
 
   constructor(private readonly _HTTP: HttpClient) {}
   
@@ -19,21 +27,21 @@ export class TmdbService {
     const url = `${this._BASE_URL}/discover/movie`;
     const params = { api_key: this._KEY, page: page.toString() };
 
-    return this._HTTP.get(url, { params });
+    return this._HTTP.get(url, { params }); //*** inclui url _DISCOVER****************************
   }
 
   getMoviesByGender(id: string): Observable<any> {
-    const url = `${this._BASE_URL}/discover/movie`;
+    const url = `${this._BASE_URL}/discover/movie`; 
     const params = { api_key: this._KEY, with_genres: id };
 
-    return this._HTTP.get(url, { params });
+    return this._HTTP.get(url, { params }); //*** inclui url _DISCOVER****************************
   }
 
   getGenderList(): Observable<any> {
     const url = `${this._BASE_URL}/genre/movie/list`;
     const params = { api_key: this._KEY };
 
-    return this._HTTP.get(url, { params });
+    return this._HTTP.get(url, { params }); //*** inclui url _GENRES******************************
   }
   
   getMoviesByPages(page: number, filters?: any, sortBy: string = 'popularity.desc'): Observable<any> {
@@ -52,18 +60,18 @@ export class TmdbService {
     // Aplicar ordenação
     params = params.set('sort_by', sortBy);
 
-    return this._HTTP.get(`${this._BASE_URL}${this._ENDPOINT}`, { params });
+    return this._HTTP.get(`${this._BASE_URL}${this._ORDER}`, { params });
   }
 
   getGenres(): Observable<any> {
     const url = `${this._BASE_URL}/genre/movie/list`;
     const params = { api_key: this._KEY };
 
-    return this._HTTP.get(url, { params });
+    return this._HTTP.get(url, { params }); //*** inclui url _GENRES*********************************
   }
   
   getMovieDetails(movieId: string = ''): Observable<any> {
-    const url = `${this._BASE_URL}${this._DETAILS_ENDPOINT}/${movieId}?api_key=${this._KEY}&language=pt-BR`;
+    const url = `${this._BASE_URL}${this._DETAILS}/${movieId}?api_key=${this._KEY}&language=pt-BR`;
 
     return this._HTTP.get(url).pipe(
       tap((data) => console.log(data)),
